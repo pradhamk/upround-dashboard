6d6cb7de479be6b2ddc2b8d2d90e1666b53d4391
pradhamk <email@pradhamk.com>
2025-05-07 00:34:04 -0400

    user whitelisting

--- app/api/auth_callback/route.ts
+++ app/api/auth_callback/route.ts
@@ -1,16 +1,32 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '@/utils/supabase/server';
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
 
   if (code) {
-    const supabase = await createClient()
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-    if (!error) {
-        return NextResponse.redirect(`${origin}`)
+    const supabase = await createClient();
+    const { data: user, error } = await supabase.auth.exchangeCodeForSession(code)
+
+    if (error) {
+      return NextResponse.redirect(`${origin}/bad_auth`)
     }
+
+    const user_email = user.user?.email;
+    const { data } = await supabase
+                    .schema('whitelist')
+                    .from('profiles')
+                    .select("*")
+                    .eq('email', user_email)
+                    .limit(1)
+                    .single();
+
+    if(!data || data.id !== user.user?.id) {
+      return NextResponse.redirect(`${origin}/not_member`)
+    }
+
+    return NextResponse.redirect(`${origin}`)
   }
 
   return NextResponse.redirect(`${origin}/bad_auth`)
