d2e5dc052960cc9bd09a0e90e7085f6e036c748d
pradhamk <email@pradhamk.com>
2025-05-20 21:49:49 -0400

    redirect url

--- app/api/redirect_oauth/route.ts
+++ app/api/redirect_oauth/route.ts
@@ -1,12 +1,19 @@
 import { createClient } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
+import { headers } from "next/headers";
 
 export async function GET() {
     const client = await createClient();
+
+    const headersList = await headers();
+    const host = headersList.get("host");
+    const protocol = host?.includes("localhost") ? "http" : "https";
+    const redirectTo = `${protocol}://${host}/api/auth_callback`;
+
     const { data } = await client.auth.signInWithOAuth({
         provider: "google",
         options: {
-            redirectTo: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/auth_callback` : "http://localhost:3000/api/auth_callback"
+            redirectTo: redirectTo
         }
     });
 
